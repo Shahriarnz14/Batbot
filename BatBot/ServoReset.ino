3745fa218341b8e6c12b39bf919b169397f7c6f1
Alex Kyriazis <alexk483@hotmail.ca>
2015-07-17 16:00:07 -0700

    updated servo reset

--- BatBot/ServoReset.ino
+++ BatBot/ServoReset.ino
@@ -1,9 +1,6 @@
-#define ALPHA 40 //angle between elbow physical and calculation 0
-#define BETA 5 //angle between shoulder physical and calculation 0
-
 void setup() {
-  LCD.print("Shoulder horizontal");
+  LCD.print("Set both arms");
   LCD.setCursor(0,1);
-  LCD.print("Elbow lowest");
-  goFastToAngles(90,180-BETA,0);
+  LCD.print("to lowest position");
+  goFastToAngles(90,0,0);
 }
